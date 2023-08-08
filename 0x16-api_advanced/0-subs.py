00069f3dfbcc45fccb28cb0fdc63e6d15edbaaee
Marvid007 <117814556+Marvid007@users.noreply.github.com>
2023-08-08 20:30:03 +0100

    Update 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,4 +1,5 @@
-ning functions for working with the Reddit API.
+#!/usr/bin/python3
+'''A module containing functions for working with the Reddit API.
 '''
 import requests
 
@@ -9,23 +10,23 @@ BASE_URL = 'https://www.reddit.com'
 
 
 def number_of_subscribers(subreddit):
-        '''Retrieves the number of subscribers in a given subreddit.
-            '''
-                api_headers = {
-                                'Accept': 'application/json',
-                                        'User-Agent': ' '.join([
-                                                        'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
-                                                                    'AppleWebKit/537.36 (KHTML, like Gecko)',
-                                                                                'Chrome/97.0.4692.71',
-                                                                                            'Safari/537.36',
-                                                                                                        'Edg/97.0.1072.62'
-                                                                                                                ])
-                                            }
-                    res = requests.get(
-                                    '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
-                                            headers=api_headers,
-                                                    allow_redirects=False
-                                                        )
-                        if res.status_code == 200:
-                                    return res.json()['data']['subscribers']
-                                    return 0
+    '''Retrieves the number of subscribers in a given subreddit.
+    '''
+    api_headers = {
+        'Accept': 'application/json',
+        'User-Agent': ' '.join([
+            'Mozilla/5.0 (Windows NT 10.0; Win64; x64)',
+            'AppleWebKit/537.36 (KHTML, like Gecko)',
+            'Chrome/97.0.4692.71',
+            'Safari/537.36',
+            'Edg/97.0.1072.62'
+        ])
+    }
+    res = requests.get(
+        '{}/r/{}/about/.json'.format(BASE_URL, subreddit),
+        headers=api_headers,
+        allow_redirects=False
+    )
+    if res.status_code == 200:
+        return res.json()['data']['subscribers']
+    return 0
